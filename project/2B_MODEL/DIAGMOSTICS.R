bf0a19ad9f2f47b67ed55f5e892467bc7648a5d2
Marco-Sulser <marco.r.sulser@student.uts.edu.au>
2020-09-26 16:22:36 +1000

    Variable Inflation Factors and Hold Out Method

--- project/2B_MODEL/DIAGMOSTICS.R
+++ project/2B_MODEL/DIAGMOSTICS.R
@@ -12,11 +12,20 @@ library (e1071)
 library (faraway)
 library (ggResidpanel)
 library (univOutl)
+library (outliers)
+
 
 # Importing data 
 
 yield_region <- read_csv(here('project/src/output/yield_by_region.csv')) %>% 
   filter(state == 'NSW' | state == 'WA' | state == 'Tas' | state =='Vic' | state =='SA') 
+
+# Spliting data in Train and Test 
+
+yield_region$id <- 1:nrow(yield_region)   
+yield_region.train <- yield_region %>% dplyr::sample_frac(.75)
+yield_region.test  <- dplyr::anti_join(yield_region, yield_region.train, by = 'id')
+
 
 
 # Ploting 
@@ -46,6 +55,8 @@ summary(yield_region)                       # stats
 
 skewness(yield_region$yield)                # skewness function [- left ; + right]
 
+grubbs.test(yield_region$yield, type = 10, opposite = FALSE, two.sided = FALSE)
+
 boxB(yield_region$yield, k=1.5, method='asymmetric', weights=NULL, id=NULL,
      exclude=NA, logt=FALSE)
 
@@ -57,11 +68,11 @@ boxB(yield_region$yield, k=1.5, method='asymmetric', weights=NULL, id=NULL,
 
 # Simple Linear Regression
 
-model_simple <- lm(yield ~ lu_water_capacity, data = yield_region)
+fit_simple <- lm(yield ~ lu_water_capacity, data = yield_region.train)
 
-summary(model_simple)
+summary(fit_simple)
 
-resid_panel(model_simple, plots = "all")
+resid_panel(fit_simple, plots = "all")
 
 
 # Multiple Linear Regression 
@@ -102,7 +113,7 @@ model <- glm(yield ~ lu_water_capacity +
 
 summary(model)
 
-resid_panel(model, plots = "all")
+ 
 
 
 # 3)
@@ -116,45 +127,65 @@ model <- glm(yield ~ lu_water_capacity +
 
 summary(model)
 
+
+
 # 4) All Variables 
 
-model_all <- glm(yield ~ 
+fit_all <- glm(yield ~ 
                
-               lu_mean_temp_annual +
-               lu_mean_temp_winter +
+               # lu_mean_temp_annual +
+               # lu_mean_temp_winter +
                lu_mean_temp_summer +
-               lu_max_temp_annual +
-               lu_max_temp_winter +
-               lu_max_temp_summer +
-               lu_min_temp_annual +
-               lu_min_temp_winter +
-               lu_min_temp_summer +
-               lu_mean_rain_annual +
-               lu_mean_rain_dry +
+               # lu_max_temp_annual +
+               # lu_max_temp_winter +
+               # lu_max_temp_summer +
+               # lu_min_temp_annual +
+               # lu_min_temp_winter +
+               # lu_min_temp_summer +
+               # lu_mean_rain_annual +
+               # lu_mean_rain_dry +
                lu_mean_rain_wet +
-               lu_mean_solar_annual +
-               lu_mean_solar_dry +
-               lu_mean_solar_wet +
+               # lu_mean_solar_annual +
+               # lu_mean_solar_dry +
+               # lu_mean_solar_wet +
                lu_bulk_density +
-               lu_carbon +
+               # lu_carbon +
                lu_clay +
                lu_nitrogen +
                lu_ph +
                lu_phosphorus +
-               lu_sand +
+               # lu_sand +
                lu_silt +
                lu_water_capacity,
              
              
+             # family = inverse.gaussian(link = "log"),
+             # family = inverse.gaussian(link = "1/mu^2"),
+             # family = Gamma(link = "inverse"),
              family = gaussian(link = "identity"),
              
-             data = yield_region)
-
-summary(model_all)
-
-resid_panel(model_all, plots = "all")
+             data = yield_region.train)
 
 
 
+summary(fit_all) # Stats
+
+vif(fit_all) # Retain model if vif (Variable Inflation Factors) of each feature is below 5. 
+
+resid_panel(fit_all, plots = "all") # Diagnostic Plots
+
+yield_region.train$residuals <- residuals(fit_all) # Calculate residuals values of model for training data 
+
+yield_region.train$predicted <- predict(fit_all, yield_region.train, type="response") # Calculate predicted values of model for training data 
+
+rmse_train <- sqrt(mean(yield_region.train$residuals ** 2)) # Root Mean Squared Error for training data 
+
+yield_region.test$predicted <- predict(fit_all, yield_region.test) # Calculate residuals values of model for training data 
+
+yield_region.test$residuals <- yield_region.test$yield - yield_region.test$predicted #  Calculate residuals values of model for test data 
+
+rmse_test <- sqrt(mean(yield_region.test$residuals ** 2)) # Root Mean Squared Error for test data 
+
+rmse_train/rmse_test # Check RMSEs shouldnt exceed 15% of each other
 
 
